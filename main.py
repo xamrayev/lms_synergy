21a8a7cbfb2cb9ab74a0057279f0ef80d83c45ee
xamrayev <xamrayevbke96@gmail.com>
2025-06-16 19:23:38 +0500

    lesson_2

--- main.py
+++ main.py
@@ -1,9 +1,16 @@
-from module_1.theme_2.lesson_1 import *
+from module_1.theme_2 import lesson_1, lesson_2
 
 def main():
-    practise_1()
+    lesson_1.practise_1()
+    
     print('\n'*3)
-    practise_2()
+    lesson_1.practise_2()
+    
+    print('\n'*3)
+    lesson_2.practise_1()
+    
+    print('\n'*3)
+    lesson_2.practise_2()
 
 if __name__ == "__main__":
     main()
