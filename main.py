57517f077aae34ded32d6a24dc9d8bd970891e9d
xamrayev <xamrayevbke96@gmail.com>
2025-06-16 19:56:17 +0500

    theme_3 lesson

--- main.py
+++ main.py
@@ -1,17 +1,10 @@
-from module_1.theme_2 import lesson_1, lesson_2, lesson_3
+from module_1.theme_3 import lesson_1
 
 def main():
-
-    # lesson_1.practise_1()
-    # lesson_1.practise_2()
+    lesson_1.practise_1()
+    lesson_1.practise_2()
+    lesson_1.practise_3()
     
-    # lesson_2.practise_1()
-    # lesson_2.practise_2()
-
-    # lesson_3.practise_1()
-    # lesson_3.practise_2()
-    lesson_3.practise_3()
-
 
 if __name__ == "__main__":
     main()
---
+++ module_1/theme_3/lesson_1.py
@@ -0,0 +1,45 @@
+def practise_1():
+    # Ввод количества чисел
+    N = int(input("Введите количество чисел: "))
+
+    count_zero = 0
+    print(f"Введите {N} целых чисел:")
+
+    # Вводим N чисел и считаем нули
+    for _ in range(N):
+        num = int(input())
+        if num == 0:
+            count_zero += 1
+
+    print("Количество нулей:", count_zero)
+
+
+def practise_2():
+    import math
+
+    # Ввод числа
+    X = int(input("Введите натуральное число X (≤ 2*10^9): "))
+
+    count = 0
+    limit = int(math.isqrt(X)) + 1
+
+    # Перебираем делители от 1 до √X
+    for i in range(1, limit):
+        if X % i == 0:
+            count += 1  # i — делитель
+            if i != X // i:
+                count += 1  # X // i — тоже делитель, если не равен i
+
+    print("Количество натуральных делителей:", count)
+
+def practise_3():
+    # Вводим A и B
+    A = int(input("Введите A (начало диапазона): "))
+    B = int(input("Введите B (конец диапазона, A ≤ B): "))
+
+    # Список чётных чисел
+    even_numbers = [str(i) for i in range(A, B + 1) if i % 2 == 0]
+
+    # Вывод через пробел
+    print("Четные числа на отрезке:")
+    print(' '.join(even_numbers))
